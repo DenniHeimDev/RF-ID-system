32fc00b511ae449119111e159712764ec8f52a21
DenniHeimDev <denniheim86@gmail.com>
2021-08-31 13:31:10 +0200

    Create inngang.py
    lagg inn discionary

---
+++ pythonScript/inngang.py
@@ -0,0 +1,12 @@
+testDict = {
+    "1234": "Dennis",
+    "4321": "Rikard",
+    "2314": "Sadner"
+}
+
+while True:
+    print("Scan kortet:")
+
+    x = input()
+    print("Velkommen: ", testDict[x])
+
