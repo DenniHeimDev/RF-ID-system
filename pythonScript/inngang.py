3b99f4d6c6670b1f0970ac9ada6a43ca87e0f055
DenniHeimDev <denniheim86@gmail.com>
2021-09-01 11:45:52 +0200

    Update inngang.py
    Timestamps

--- pythonScript/inngang.py
+++ pythonScript/inngang.py
@@ -1,12 +1,19 @@
-testDict = {
+from datetime import datetime
+
+idDict = {
     "1234": "Dennis",
     "4321": "Rikard",
     "2314": "Sadner"
 }
 
+now = datetime.now()
+print(now)
+timestamp = datetime.timestamp(now)
+print("timestamp =", timestamp)
+
 while True:
     print("Scan kortet:")
 
     x = input()
-    print("Velkommen: ", testDict[x])
+    print("Velkommen: ", idDict[x], ".", "Du kom klokka ", datetime.now())
 
