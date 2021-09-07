8377fbcb2d63a2ee3cfb804fd6115a2b1b730fc8
DenniHeimDev <denniheim86@gmail.com>
2021-09-07 09:06:49 +0200

    Update inngang.py

--- pythonScript/inngang.py
+++ pythonScript/inngang.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+
+
 idDict = {
     "1234": "Dennis",
     "4321": "Rikard",
@@ -13,8 +15,12 @@ print("timestamp =", timestamp)
 
 while True:
     print("Scan kortet:")
+    navn = ""
 
     x = input()
 
-    print("Velkommen: ", idDict[x], ".", "Du kom klokka ", datetime.now())
+    navn = idDict[x]
+
+    print("Velkommen: %s. Du kom klokka %s" % (navn, datetime.now()))
+    #print("Velkommen: ", navn, ".", "Du kom klokka ", datetime.now())
 
